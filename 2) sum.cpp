4f459bcfff88ee22861f41c95ca8a9725e286228
Animesh Acharya <animeshacharya867+github@gmail.com>
2022-11-09 20:43:22 +0545

    Added some new files and a new repository

---
+++ 2) sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,sum;
+    cout<<"Enter any two numbers:"<<endl;
+    cin>>a>>b;
+    sum=a+b;
+    cout<<sum<<" is the sum of the two numbers "<<a<<" "<<b <<endl;
+    return 0;
+}
